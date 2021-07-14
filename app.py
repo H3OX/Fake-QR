5340bbd989a89962127c2de9de73eac1de722d96
H3OX <piankovg@gmail.com>
2021-07-15 01:22:05 +0300

    init11

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os.path
 from flask import Flask, render_template
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__, template_folder='static/templates/')
 
@@ -7,6 +8,6 @@ app = Flask(__name__, template_folder='static/templates/')
 @app.route('/', methods=['GET'])
 def root():
     return render_template('index.html')
-    
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=port)
