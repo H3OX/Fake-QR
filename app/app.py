9492b9cf486cc210c58fbd3cd76c6953191eac86
H3OX <piankovg@gmail.com>
2021-07-15 00:49:53 +0300

    init2

--- app/app.py
+++ app/app.py
@@ -8,5 +8,3 @@ app = Flask(__name__, template_folder='static/templates/')
 def root():
     return render_template('index.html')
     
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
